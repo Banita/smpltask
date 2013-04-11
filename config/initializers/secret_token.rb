a9ddfa9aed91aa94ae106166defe36236d3c0d08
Banita Patro <banita.patro@strata.co.in>
2013-04-11 22:01:35 +0530

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleTask::Application.config.secret_token = '3951a3533913f4a6b6e2f8727edd81fd81e5c328f1cfff88015669c67980a7ad6ac227d47735aa300d0c2ff8211ffe19a978c6c480333b892da3c27d5053c572'
