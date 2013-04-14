664f1f846e846870223df1706511c13a0b9aebb9
Banita Patro <banita.patro@strata.co.in>
2013-04-14 10:14:14 +0530

    secret code index page changes

--- app/controllers/secret_code_controller.rb
+++ app/controllers/secret_code_controller.rb
@@ -1,10 +1,9 @@
 class SecretCodeController < ApplicationController
 	def index
-	#@Secret_codes=SecretCode.all	
+	  @secret_codes=SecretCode.all	
 	end
 
 	def create
-		@secret_code=SecretCode.generate_codes(params[:number])
-	
+	  @secret_code=SecretCode.generate_codes(params[:number])
 	end
 end
